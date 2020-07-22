35ef3479ac47e76a240aca43a39ec70a405b51ea
Caique Deodato <caique.j.deodato@gmail.com>
2020-07-22 12:50:39 -0300

    Acrescentada uma mensagem de bom dia (em ingles)

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -5,6 +5,7 @@ namespace projeto_teste {
         static void Main(string[] args) {
 
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Good Morning!");
 
             Console.ReadLine();
         }
