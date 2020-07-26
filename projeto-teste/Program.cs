a3716a26344d197072deed01d4c3fcfdd87a7d33
Caique Deodato <caique.j.deodato@gmail.com>
2020-07-26 11:57:22 -0300

    Acrescentada a pergunta sobre como voce esta

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -6,6 +6,7 @@ namespace projeto_teste {
 
             Console.WriteLine("Hello World!");
             Console.WriteLine("Good Morning!");
+            Console.WriteLine("How you doing today?");
 
             Console.ReadLine();
         }
